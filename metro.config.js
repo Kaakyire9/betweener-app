bd138cf2bee1db15a8dd034c0501bfa7d741ed4d
Kaakyire9 <r.aduboffour@gmail.com>
2026-02-12 20:53:17 +0000

    fix: disable metro package exports to unblock sentry bundling

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,12 @@
 // Safe to keep even when Sentry isn't configured; it will be a no-op unless @sentry/react-native is installed.
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
-module.exports = getSentryExpoConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
+// Work around Metro package-exports resolution issues some ESM packages hit on Expo/Metro.
+// This keeps Metro on the classic mainFields resolver path (react-native/browser/main).
+// If you later upgrade Expo/Sentry and want to try package exports again, flip this back to true.
+config.resolver = config.resolver || {};
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
